82f18cdbdf45293306271e5795d60d910e178e36
Ayaz Ayupov <ayaz@releaseband.com>
2021-03-19 12:22:04 +0300

    prepare: added cobra, added command run for start

---
+++ generator/generator.go
@@ -0,0 +1,5 @@
+package generator
+
+func Run(path string) error {
+	return nil
+}
--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/releaseband/map-gen/cli/cmd"
+	"log"
+)
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
