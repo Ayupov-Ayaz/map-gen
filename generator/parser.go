e2beb23cc992cd6324db0204814c8c8cdf640275
Ayaz Ayupov <ayaz@releaseband.com>
2021-03-19 14:18:09 +0300

    parsing comments

--- generator/parser.go
+++ generator/parser.go
@@ -3,7 +3,10 @@ package generator
 import (
 	"errors"
 	"go/ast"
+	"strings"
 )
+
+const mapGenCommentTag = "//map_gen"
 
 func parseImports(d *ast.GenDecl) ([]string, error) {
 	imports := make([]string, len(d.Specs))
@@ -17,4 +20,57 @@ func parseImports(d *ast.GenDecl) ([]string, error) {
 	}
 
 	return imports, nil
+}
+
+func parseMapGenComments(comments []*ast.Comment) bool {
+	for _, c := range comments {
+		if strings.HasPrefix(c.Text, mapGenCommentTag) {
+
+		}
+	}
+
+	return false
+}
+
+func getCommentFromSingleVar(decl *ast.GenDecl) []*ast.Comment {
+	if decl.Doc != nil && len(decl.Doc.List) > 0 {
+		return decl.Doc.List
+	}
+
+	return nil
+}
+
+func getCommentFromMultiVar(decl *ast.GenDecl) []*ast.Comment {
+	if len(decl.Specs) == 0 {
+		return nil
+	}
+
+	var comments []*ast.Comment
+
+	for _, s := range decl.Specs {
+		vs, ok := s.(*ast.ValueSpec)
+		if !ok || vs.Doc == nil || vs.Doc.List == nil {
+			continue
+		}
+
+		comments = append(comments, vs.Doc.List...)
+	}
+
+	return comments
+}
+
+func getComments(decl *ast.GenDecl) []*ast.Comment {
+	comments := getCommentFromSingleVar(decl)
+	if comments != nil {
+		return comments
+	}
+
+	return getCommentFromMultiVar(decl)
+}
+
+func parseVars(decl *ast.GenDecl) ([]Variant, error) {
+	comments := getComments(decl)
+	parseMapGenComments(comments)
+
+	return nil, nil
 }
